21d58e4ed4d0c691a0d18e45800a7e824d82fc3f
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-08-06 17:24:19 +0200

    Added css

--- src/City.js
+++ src/City.js
@@ -1,12 +1,13 @@
 import React from "react";
 import madrid from "./madrid.jpg";
+import "./City.css";
 
 export default function City() {
   return (
     <div className="City">
       <div className="row">
         <div className="col-3">
-          <img src={madrid} alt="Madrid" width="200px" />
+          <img src={madrid} alt="Madrid" className="img-fluid rounded" />
         </div>
         <div className="col-9">
           <h4>Spain</h4>
