c87190192523dc71243a2405c037eca187ce6506
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-08-06 19:13:48 +0200

    Added css

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,12 @@
 import React from "react";
+import "./NavBar.css";
+import map from "./map.svg";
 
 export default function NavBar() {
-  return <div className="NavBar">Travel Journal</div>;
+  return (
+    <div className="NavBar">
+      <h3>Travel Journel</h3>
+      <img src={map} alt="map" width="90px" className="img-fluid map" />
+    </div>
+  );
 }
