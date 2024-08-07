3efcb804f7f351e6aae3046431e8951c1ee0cb83
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-08-07 17:08:46 +0200

    updated NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,8 +5,14 @@ import map from "./map.svg";
 export default function NavBar() {
   return (
     <div className="NavBar">
-      <h3>Travel Journel</h3>
-      <img src={map} alt="map" width="90px" className="img-fluid map" />
+      <div className="grid">
+        <div>
+          <h3>Travel Journel</h3>
+        </div>
+        <div>
+          <img src={map} alt="map" width="90px" className="img-fluid map" />
+        </div>
+      </div>
     </div>
   );
 }
